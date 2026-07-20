b113f26b7de879e9896ea578ea59813af8335390
SriBairava <sribairava001@gmail.com>
2026-07-20 11:10:07 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -0,0 +1,18 @@
+// Last updated: 20/07/2026, 11:10:07
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) return true;
+        int len = flowerbed.length;
+        for (int i = 0; i < len; i++) {
+            if (flowerbed[i] == 0 &&
+                (i == 0 || flowerbed[i - 1] == 0) &&
+                (i == len - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                if (--n == 0)
+                    return true;
+                i++; 
+            }
+        }
+        return false;
+    }
+}
