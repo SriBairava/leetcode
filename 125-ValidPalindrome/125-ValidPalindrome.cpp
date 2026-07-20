5b1f348f320f1cb43f47f9653ec63d0b4d2b01e4
SriBairava <sribairava001@gmail.com>
2026-07-20 11:11:39 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -0,0 +1,26 @@
+// Last updated: 20/07/2026, 11:11:39
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
