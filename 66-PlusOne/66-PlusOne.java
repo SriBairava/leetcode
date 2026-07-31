957596bc4c1114115dee13a52973c190be2493b6
SriBairava <sribairava001@gmail.com>
2026-07-31 09:23:10 +0530

    File updated at 31/07/2026, 09:23:10

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,16 +1,16 @@
-// Last updated: 31/07/2026, 09:22:32
+// Last updated: 31/07/2026, 09:23:10
 1class Solution {
-2    public int[] plusOne(int[] digits) {
-3        for (int i = digits.length - 1; i >= 0; i--) {
-4            if (digits[i] + 1 != 10) {
-5                digits[i] += 1;
-6                return digits;
-7            }
-8            digits[i] = 0;
-9        }
-10        
-11        int[] newDigits = new int[digits.length + 1];
-12        newDigits[0] = 1;
-13        return newDigits;        
+2    public ListNode deleteDuplicates(ListNode head) {
+3        ListNode res = head;
+4
+5        while (head != null && head.next != null) {
+6            if (head.val == head.next.val) {
+7                head.next = head.next.next;
+8            } else {
+9                head = head.next;
+10            }
+11        }
+12
+13        return res;        
 14    }
 15}
