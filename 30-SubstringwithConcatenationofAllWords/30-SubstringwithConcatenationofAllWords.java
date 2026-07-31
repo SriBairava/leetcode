3ab79d31ba20bcb3e12be74da1693a86b5d6a676
SriBairava <sribairava001@gmail.com>
2026-07-31 09:07:45 +0530

    File updated at 31/07/2026, 09:07:44

--- 30-SubstringwithConcatenationofAllWords/30-SubstringwithConcatenationofAllWords.java
+++ 30-SubstringwithConcatenationofAllWords/30-SubstringwithConcatenationofAllWords.java
@@ -1,17 +1,22 @@
-// Last updated: 31/07/2026, 09:06:17
-1class Solution {
-2    public int lengthOfLastWord(String s) {
-3        int end = s.length() - 1;
-4
-5        while (end >= 0 && s.charAt(end) == ' ') {
-6            end--;
-7        }
-8
-9        int start = end;
-10        while (start >= 0 && s.charAt(start) != ' ') {
-11            start--;
-12        }
-13
-14        return end - start;        
-15    }
-16}
+// Last updated: 31/07/2026, 09:07:44
+1class Solution 
+2{
+3  public String addBinary(String a, String b) 
+4  {
+5    StringBuilder sb = new StringBuilder();
+6    int carry = 0;
+7    int i = a.length() - 1;
+8    int j = b.length() - 1;
+9
+10    while (i >= 0 || j >= 0 || carry == 1) 
+11    {
+12      if(i >= 0)
+13        carry += a.charAt(i--) - '0';
+14      if(j >= 0)
+15        carry += b.charAt(j--) - '0';
+16      sb.append(carry % 2);
+17      carry /= 2;
+18    }
+19    return sb.reverse().toString();
+20  }
+21}
