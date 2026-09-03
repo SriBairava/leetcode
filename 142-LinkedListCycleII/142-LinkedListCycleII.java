d06397942acf1791dfd91eef504d3ab9edcbd02e
SriBairava <sribairava001@gmail.com>
2026-09-03 09:12:38 +0530

    File updated at 03/09/2026, 09:12:38

--- 142-LinkedListCycleII/142-LinkedListCycleII.java
+++ 142-LinkedListCycleII/142-LinkedListCycleII.java
@@ -1,28 +1,24 @@
-// Last updated: 03/09/2026, 09:11:59
+// Last updated: 03/09/2026, 09:12:38
 1class Solution {
-2    public String simplifyPath(String path) {
-3        String[] components = path.split("/");
-4        Stack<String> st = new Stack<>();
-5
-6        for (String comp : components) {
-7            if (comp.equals("") || comp.equals(".")) {
-8                continue;
-9            }
+2  public int minDistance(String word1, String word2) {
+3    final int m = word1.length();//first word length
+4    final int n = word2.length();///second word length
+5    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
+6    int[][] dp = new int[m + 1][n + 1];
+7
+8    for (int i = 1; i <= m; ++i)
+9      dp[i][0] = i;
 10
-11            if (comp.equals("..")) {
-12                if (!st.isEmpty()) {
-13                    st.pop();
-14                }
-15            } else {
-16                st.push(comp);
-17            }
-18        }
-19
-20        StringBuilder sb = new StringBuilder();
-21        while (!st.isEmpty()) {
-22            sb.insert(0, "/" + st.pop());
-23        }
-24
-25        return sb.length() == 0 ? "/" : sb.toString();        
-26    }
-27}
+11    for (int j = 1; j <= n; ++j)
+12      dp[0][j] = j;
+13
+14    for (int i = 1; i <= m; ++i)
+15      for (int j = 1; j <= n; ++j)
+16        if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
+17          dp[i][j] = dp[i - 1][j - 1];//no operation
+18        else
+19          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                      //replace               //delete        //insert
+20
+21    return dp[m][n];
+22  }
+23}
