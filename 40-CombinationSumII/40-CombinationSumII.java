f790a58e4634da236adabcf179debb5011a9662d
SriBairava <sribairava001@gmail.com>
2026-09-03 10:25:46 +0530

    File updated at 03/09/2026, 10:25:45

--- 40-CombinationSumII/40-CombinationSumII.java
+++ 40-CombinationSumII/40-CombinationSumII.java
@@ -1,18 +1,18 @@
-// Last updated: 03/09/2026, 10:25:15
+// Last updated: 03/09/2026, 10:25:45
 1class Solution {
-2    public int jump(int[] nums) {
-3        int near = 0, far = 0, jumps = 0;
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        Map<String, List<String>> ans = new HashMap<>();
 4
-5        while (far < nums.length - 1) {
-6            int farthest = 0;
-7            for (int i = near; i <= far; i++) {
-8                farthest = Math.max(farthest, i + nums[i]);
-9            }
-10            near = far + 1;
-11            far = farthest;
-12            jumps++;
+5        for (String s : strs) {
+6            char[] chars = s.toCharArray();
+7            Arrays.sort(chars);
+8            String key = new String(chars);
+9            if (!ans.containsKey(key)) {
+10                ans.put(key, new ArrayList<>());
+11            }
+12            ans.get(key).add(s);
 13        }
 14
-15        return jumps;        
+15        return new ArrayList<>(ans.values());        
 16    }
 17}
