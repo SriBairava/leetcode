639d69afb6399af57a01c2916291587e196f5391
SriBairava <sribairava001@gmail.com>
2026-09-21 22:07:22 +0530

    File updated at 21/09/2026, 22:07:21

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,29 +1,18 @@
-// Last updated: 21/09/2026, 22:05:08
+// Last updated: 21/09/2026, 22:07:21
 1class Solution {
-2    public int longestValidParentheses(String s) {
-3        if(s.equals("")){
-4            return 0;
-5        }
-6
-7        Stack<Integer> st = new Stack<>();
-8        st.push(-1);
-9
-10        int max_len = 0;
-11
-12        for(int i=0;i<s.length();i++){
-13            if(s.charAt(i) == '('){
-14                st.push(i);
-15            }
-16            else{
-17                st.pop();
-18                if(st.isEmpty()){
-19                    st.push(i);
-20                }
-21                else{
-22                    max_len = Math.max(max_len,i - st.peek());
-23                }
-24            }
-25        }
-26        return max_len;
-27    }
-28}
+2    public int maxSubArray(int[] nums) {
+3        int res = nums[0];
+4        int total = 0;
+5
+6        for (int n : nums) {
+7            if (total < 0) {
+8                total = 0;
+9            }
+10
+11            total += n;
+12            res = Math.max(res, total);
+13        }
+14
+15        return res;        
+16    }
+17}
