46afbd43279e68b47e3986c41fe0166afb51d0a9
SriBairava <sribairava001@gmail.com>
2026-09-21 22:10:54 +0530

    File updated at 21/09/2026, 22:10:54

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,25 +1,35 @@
-// Last updated: 21/09/2026, 22:10:00
+// Last updated: 21/09/2026, 22:10:54
 1class Solution {
-2    public int[][] insert(int[][] intervals, int[] newInterval) {
-3       List<int[]> intervalList = new ArrayList<>(Arrays.asList(intervals));
-4        intervalList.add(newInterval);
-5        Collections.sort(intervalList, (a, b) -> Integer.compare(a[0], b[0]));
-6
-7        List<int[]> res = new ArrayList<>();
-8        int[] current = intervalList.get(0);
-9
-10        for (int i = 1; i < intervalList.size(); i++) {
-11            int[] interval = intervalList.get(i);
-12            
-13            if (current[1] >= interval[0]) {
-14                current[1] = Math.max(current[1], interval[1]);
-15            } else {
-16                res.add(current);
-17                current = interval;
-18            }
-19        }
+2
+3    public String getPermutation(int n, int k) {
+4
+5        int fact = 1;
+6        List<Integer> numbers = new ArrayList<>();
+7
+8        for (int i = 1; i < n; i++) {
+9            fact = fact * i;
+10            numbers.add(i);
+11        }
+12
+13        numbers.add(n);
+14
+15        String ans = "";
+16
+17        k = k - 1;
+18
+19        while (true) {
 20
-21        res.add(current);
-22        return res.toArray(new int[res.size()][]);
-23    }
-24}
+21            ans += numbers.get(k / fact);
+22            numbers.remove(k / fact);
+23
+24            if (numbers.size() == 0) {
+25                break;
+26            }
+27
+28            k = k % fact;
+29            fact = fact / numbers.size();
+30        }
+31
+32        return ans;
+33    }
+34}
