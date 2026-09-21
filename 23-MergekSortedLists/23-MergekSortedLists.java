d8b1b295ae7c103cd429f007c7edf7e02b2ffe3a
SriBairava <sribairava001@gmail.com>
2026-09-21 22:17:59 +0530

    File updated at 21/09/2026, 22:17:58

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,36 +1,42 @@
-// Last updated: 21/09/2026, 22:17:31
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode partition(ListNode head, int x) {
-13        ListNode slist = new ListNode();
-14        ListNode blist = new ListNode();
-15        ListNode small = slist;
-16        ListNode big = blist;
-17
-18        while (head != null) {
-19            if (head.val < x) {
-20                small.next = head;
-21                small = small.next;
-22            } else {
-23                big.next = head;
-24                big = big.next;
-25            }
-26
-27            head = head.next;
-28        }
-29
-30        small.next = blist.next;
-31        big.next = null;
-32
-33        return slist.next;        
-34    }
-35}
+// Last updated: 21/09/2026, 22:17:58
+1class Solution {
+2    // to store previously computed substrings
+3    Map<String, Boolean> map = new HashMap<>();
+4
+5    public boolean isScramble(String s1, String s2) {
+6        int n = s1.length();
+7        // check if the two strings are equal
+8        if (s1.equals(s2)) {
+9            return true;
+10        }
+11        // initialize frequency arrays for s1, s2, and current substring
+12        int[] a = new int[26], b = new int[26], c = new int[26];
+13        // check if the current substring has already been computed
+14        if (map.containsKey(s1 + s2)) {
+15            return map.get(s1 + s2);
+16        }
+17        // check all possible splits of the two strings
+18        for (int i = 1; i <= n - 1; i++) {
+19            int j = n - i;
+20            // update frequency arrays for s1, s2, and current substring
+21            a[s1.charAt(i - 1) - 'a']++;
+22            b[s2.charAt(i - 1) - 'a']++;
+23            c[s2.charAt(j) - 'a']++;
+24            // check if the current substring has the same characters
+25            if (Arrays.equals(a, b) && isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i))) {
+26                // if the substrings are scrambled versions of each other, return true
+27                map.put(s1 + s2, true);
+28                return true;
+29            }
+30            // check if the current substring and its complement have the same characters
+31            if (Arrays.equals(a, c) && isScramble(s1.substring(0, i), s2.substring(j)) && isScramble(s1.substring(i), s2.substring(0, j))) {
+32                // if the substrings are scrambled versions of each other, return true
+33                map.put(s1 + s2, true);
+34                return true;
+35            }
+36        }
+37        // if none of the splits result in scrambled versions, return false
+38        map.put(s1 + s2, false);
+39        return false;
+40    }
+41}
