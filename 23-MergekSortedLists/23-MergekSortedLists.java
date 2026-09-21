1164395fd684c0b277535c34f737ec1a7a6400f1
SriBairava <sribairava001@gmail.com>
2026-09-21 22:13:16 +0530

    File updated at 21/09/2026, 22:13:16

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,24 +1,19 @@
-// Last updated: 21/09/2026, 22:12:30
+// Last updated: 21/09/2026, 22:13:16
 1class Solution {
-2    public boolean isNumber(String s) {
-3        boolean isdot = false, ise = false, nums = false;
-4        for (int i = 0; i < s.length(); i++) {
-5            char c = s.charAt(i);
-6            if (Character.isDigit(c)) nums = true;
-7            else if (c == '+' || c == '-') {
-8                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
-9            } 
-10            else if (c == 'e' || c == 'E') {
-11                if (ise || !nums) return false;
-12                ise = true;
-13                nums = false;
-14            } 
-15            else if (c == '.') {
-16                if (isdot || ise) return false;
-17                isdot = true;
-18            } 
-19            else return false;
-20        }
-21        return nums;
-22    }
-23}
+2        public static List<List<Integer>> combine(int n, int k) {
+3		List<List<Integer>> combs = new ArrayList<List<Integer>>();
+4		combine(combs, new ArrayList<Integer>(), 1, n, k);
+5		return combs;
+6	}
+7	public static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
+8		if(k==0) {
+9			combs.add(new ArrayList<Integer>(comb));
+10			return;
+11		}
+12		for(int i=start;i<=n;i++) {
+13			comb.add(i);
+14			combine(combs, comb, i+1, n, k-1);
+15			comb.remove(comb.size()-1);
+16		}
+17	}
+18}
