038ca3462247c083cbc87478ce772bd11ba749a2
SriBairava <sribairava001@gmail.com>
2026-09-21 22:22:44 +0530

    File updated at 21/09/2026, 22:22:44

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,41 +1,45 @@
-// Last updated: 21/09/2026, 22:22:17
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public int numDistinct(String S, String T) {
-18    // array creation
-19    int[][] mem = new int[T.length()+1][S.length()+1];
-20
-21    // filling the first row: with 1s
-22    for(int j=0; j<=S.length(); j++) {
-23        mem[0][j] = 1;
-24    }
-25    
-26    // the first column is 0 by default in every other rows but the first, which we need.
-27    
-28    for(int i=0; i<T.length(); i++) {
-29        for(int j=0; j<S.length(); j++) {
-30            if(T.charAt(i) == S.charAt(j)) {
-31                mem[i+1][j+1] = mem[i][j] + mem[i+1][j];
-32            } else {
-33                mem[i+1][j+1] = mem[i+1][j];
-34            }
-35        }
-36    }
-37    
-38    return mem[T.length()][S.length()];
-39}
-40}
+// Last updated: 21/09/2026, 22:22:44
+1/* 
+2// Definition for a Node.
+3class Node {
+4    public int val;
+5    public Node left;
+6    public Node right;
+7    public Node next;
+8
+9    public Node() {}
+10    public Node(int _val) { val = _val; }
+11    public Node(int _val, Node _left, Node _right, Node _next) {
+12        val = _val;
+13        left = _left;
+14        right = _right;
+15        next = _next;
+16    }
+17};
+18*/
+19
+20class Solution {
+21    public Node connect(Node root) {
+22        if (root == null) return root;
+23
+24        Node leftMost = root;
+25
+26        // Traverse level by level
+27        while (leftMost.left != null) {
+28            Node curr = leftMost;
+29            while (curr != null) {
+30                // Connect left -> right
+31                curr.left.next = curr.right;
+32
+33                // Connect right -> next's left (if available)
+34                if (curr.next != null) {
+35                    curr.right.next = curr.next.left;
+36                }
+37
+38                curr = curr.next; // Move to next node in the level
+39            }
+40            leftMost = leftMost.left; // Go to next level
+41        }
+42        return root;
+43    }
+44}
