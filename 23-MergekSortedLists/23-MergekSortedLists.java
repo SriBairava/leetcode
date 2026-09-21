e07740ee4689365b3f5d4766ec69259474bbfacc
SriBairava <sribairava001@gmail.com>
2026-09-21 22:14:31 +0530

    File updated at 21/09/2026, 22:14:30

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,19 +1,23 @@
-// Last updated: 21/09/2026, 22:13:16
+// Last updated: 21/09/2026, 22:14:30
 1class Solution {
-2        public static List<List<Integer>> combine(int n, int k) {
-3		List<List<Integer>> combs = new ArrayList<List<Integer>>();
-4		combine(combs, new ArrayList<Integer>(), 1, n, k);
-5		return combs;
-6	}
-7	public static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
-8		if(k==0) {
-9			combs.add(new ArrayList<Integer>(comb));
-10			return;
-11		}
-12		for(int i=start;i<=n;i++) {
-13			comb.add(i);
-14			combine(combs, comb, i+1, n, k-1);
-15			comb.remove(comb.size()-1);
-16		}
-17	}
-18}
+2    public List<List<Integer>> subsets(int[] nums) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        List<Integer> subset = new ArrayList<>();
+5
+6        createSubset(nums, 0, res, subset);
+7        return res;        
+8    }
+9
+10    private void createSubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
+11        if (index == nums.length) {
+12            res.add(new ArrayList<>(subset));
+13            return;
+14        }
+15
+16        subset.add(nums[index]);
+17        createSubset(nums, index + 1, res, subset);
+18
+19        subset.remove(subset.size() - 1);
+20        createSubset(nums, index + 1, res, subset);
+21    }    
+22}
