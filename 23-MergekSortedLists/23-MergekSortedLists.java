3c97e03e803d8fd893f11f999048d44dfee9491a
SriBairava <sribairava001@gmail.com>
2026-09-21 22:20:15 +0530

    File updated at 21/09/2026, 22:20:15

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,35 +1,31 @@
-// Last updated: 21/09/2026, 22:19:39
-1class Solution {
-2    public int numDecodings(String s) {
-3        int n = s.length();
-4        if (n == 1 || s.charAt(0) == '0') {
-5            return s.charAt(0) >= '1' && s.charAt(0) <= '9' ? 1 : 0;
-6        }
-7
-8        int[] dp = new int[n];
-9
-10        int d1 = s.charAt(0) - '0';
-11        int d2 = s.charAt(1) - '0';
-12
-13        dp[0] = d1 >= 1 && d1 <= 9 ? 1 : 0;
-14
-15        dp[1] += d2 >= 1 && d2 <= 9 ? 1 : 0;
-16        dp[1] += d1 * 10 + d2 >= 1 && d1 * 10 + d2 <= 26 && d1 != 0 ? 1 : 0;
-17
-18        for (int i = 2; i < n; i++) {
-19            d1 = s.charAt(i - 1) - '0';
-20            d2 = s.charAt(i) - '0';
-21
-22            int num = d1 * 10 + d2;
-23
-24            int sum = (d2 >= 1 && d2 <= 9 ? dp[i - 1] : 0);
-25            sum += (num >= 1 && num <= 26 && d1 != 0 ? dp[i - 2] : 0);
-26
-27            dp[i] = sum;
-28
-29            if (dp[i] == 0) return 0;
-30        }
-31
-32        return dp[n - 1];
-33    }
-34}
+// Last updated: 21/09/2026, 22:20:15
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode sortedArrayToBST(int[] nums) {
+18        return build(nums, 0, nums.length - 1);
+19    }
+20    private TreeNode build(int[] nums, int left, int right) {
+21        if (left > right) {
+22            return null;
+23        }
+24        int mid = left + (right - left) / 2;
+25        TreeNode root = new TreeNode(nums[mid]);
+26        root.left = build(nums, left, mid - 1);
+27        root.right = build(nums, mid + 1, right);
+28        return root;
+29    }
+30}
