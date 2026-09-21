eff700514260376da6fcfb2e736ea28a47129d84
SriBairava <sribairava001@gmail.com>
2026-09-21 22:15:15 +0530

    File updated at 21/09/2026, 22:15:14

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,23 +1,17 @@
-// Last updated: 21/09/2026, 22:14:30
+// Last updated: 21/09/2026, 22:15:14
 1class Solution {
-2    public List<List<Integer>> subsets(int[] nums) {
-3        List<List<Integer>> res = new ArrayList<>();
-4        List<Integer> subset = new ArrayList<>();
+2    public int removeDuplicates(int[] nums) {
+3        Map<Integer, Integer> count = new HashMap<>();
+4        int k = 0;
 5
-6        createSubset(nums, 0, res, subset);
-7        return res;        
-8    }
-9
-10    private void createSubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
-11        if (index == nums.length) {
-12            res.add(new ArrayList<>(subset));
-13            return;
-14        }
-15
-16        subset.add(nums[index]);
-17        createSubset(nums, index + 1, res, subset);
-18
-19        subset.remove(subset.size() - 1);
-20        createSubset(nums, index + 1, res, subset);
-21    }    
-22}
+6        for (int num : nums) {
+7            count.put(num, count.getOrDefault(num, 0) + 1);
+8            if (count.get(num) <= 2) {
+9                nums[k] = num;
+10                k++;
+11            }
+12        }
+13
+14        return k;      
+15    }
+16}
