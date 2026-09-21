4f6416bb7470d2601131c40dba7e2cb28b2a9be4
SriBairava <sribairava001@gmail.com>
2026-09-21 22:10:00 +0530

    File updated at 21/09/2026, 22:10:00

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,23 +1,25 @@
-// Last updated: 21/09/2026, 22:09:21
+// Last updated: 21/09/2026, 22:10:00
 1class Solution {
-2    public int[][] merge(int[][] intervals) {
-3        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-4
-5        List<int[]> merged = new ArrayList<>();
-6        int[] prev = intervals[0];
-7
-8        for (int i = 1; i < intervals.length; i++) {
-9            int[] interval = intervals[i];
-10            if (interval[0] <= prev[1]) {
-11                prev[1] = Math.max(prev[1], interval[1]);
-12            } else {
-13                merged.add(prev);
-14                prev = interval;
-15            }
-16        }
-17
-18        merged.add(prev);
-19
-20        return merged.toArray(new int[merged.size()][]);         
-21    }
-22}
+2    public int[][] insert(int[][] intervals, int[] newInterval) {
+3       List<int[]> intervalList = new ArrayList<>(Arrays.asList(intervals));
+4        intervalList.add(newInterval);
+5        Collections.sort(intervalList, (a, b) -> Integer.compare(a[0], b[0]));
+6
+7        List<int[]> res = new ArrayList<>();
+8        int[] current = intervalList.get(0);
+9
+10        for (int i = 1; i < intervalList.size(); i++) {
+11            int[] interval = intervalList.get(i);
+12            
+13            if (current[1] >= interval[0]) {
+14                current[1] = Math.max(current[1], interval[1]);
+15            } else {
+16                res.add(current);
+17                current = interval;
+18            }
+19        }
+20
+21        res.add(current);
+22        return res.toArray(new int[res.size()][]);
+23    }
+24}
