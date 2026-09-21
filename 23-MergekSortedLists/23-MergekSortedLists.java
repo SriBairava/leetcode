b508df27c104fcb1bfab87d3d4df3445e32682fa
SriBairava <sribairava001@gmail.com>
2026-09-21 22:29:08 +0530

    File updated at 21/09/2026, 22:29:07

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,26 +1,36 @@
-// Last updated: 21/09/2026, 22:27:52
-1class Solution {
-2    List<String> results;
-3    HashSet<String> dict;
-4    public List<String> wordBreak(String s, List<String> wordDict) {
-5        results = new ArrayList<>();
-6        dict = new HashSet<String>();
-7        for(String word: wordDict)dict.add(word);
-8        backTrack(s,0,new StringBuilder());
-9        return results;
-10        
-11    }
-12    
-13    public void backTrack(String s, int start, StringBuilder currentSentence){
-14        if(start==s.length()) results.add(currentSentence.toString().trim());
-15        for(int i = start; i<s.length();i++){
-16            if(dict.contains(s.substring(start,i+1))) {
-17                int len = currentSentence.length();
-18                currentSentence.append(s.substring(start,i+1));
-19                currentSentence.append(" ");
-20                backTrack(s,i+1,currentSentence);
-21                currentSentence.setLength(len);
-22            }
-23        }
-24    }
-25}
+// Last updated: 21/09/2026, 22:29:07
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode sortList(ListNode head) {
+13        if (head == null) return null;
+14
+15        ArrayList<Integer> list = new ArrayList<>();
+16
+17      
+18        ListNode temp = head;
+19        while (temp != null) {
+20            list.add(temp.val);
+21            temp = temp.next;
+22        }
+23       
+24        Collections.sort(list);
+25        temp = head;
+26        int i = 0;
+27        while (temp != null) {
+28            temp.val = list.get(i++);
+29            temp = temp.next;
+30        }
+31
+32        return head;
+33    }
+34}
+35 
