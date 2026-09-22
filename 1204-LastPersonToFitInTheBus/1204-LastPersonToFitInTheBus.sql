57a55d6727fd52579069ff3b627f8c9aab55aa36
SriBairava <sribairava001@gmail.com>
2026-09-22 09:19:05 +0530

    Create 1204-LastPersonToFitInTheBus

---
+++ 1204-LastPersonToFitInTheBus/1204-LastPersonToFitInTheBus.sql
@@ -0,0 +1,15 @@
+-- Last updated: 22/09/2026, 09:19:04
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+WITH new_table AS (
+    SELECT 
+        person_name,
+        SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
+    FROM Queue
+)
+SELECT 
+    person_name
+FROM new_table
+WHERE cumulative_weight <= 1000
+ORDER BY cumulative_weight DESC
+LIMIT 1;
