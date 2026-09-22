7146e1e0d5c9b07fdebb75b8d61d65e2c82428bb
SriBairava <sribairava001@gmail.com>
2026-09-22 09:22:05 +0530

    Create 115-DistinctSubsequences

---
+++ 115-DistinctSubsequences/115-DistinctSubsequences.java
@@ -0,0 +1,41 @@
+// Last updated: 22/09/2026, 09:22:05
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int numDistinct(String S, String T) {
+    // array creation
+    int[][] mem = new int[T.length()+1][S.length()+1];
+
+    // filling the first row: with 1s
+    for(int j=0; j<=S.length(); j++) {
+        mem[0][j] = 1;
+    }
+    
+    // the first column is 0 by default in every other rows but the first, which we need.
+    
+    for(int i=0; i<T.length(); i++) {
+        for(int j=0; j<S.length(); j++) {
+            if(T.charAt(i) == S.charAt(j)) {
+                mem[i+1][j+1] = mem[i][j] + mem[i+1][j];
+            } else {
+                mem[i+1][j+1] = mem[i+1][j];
+            }
+        }
+    }
+    
+    return mem[T.length()][S.length()];
+}
+}
