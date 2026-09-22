d4123b4895351fdea56e82f3aae7e2d5b7f49101
SriBairava <sribairava001@gmail.com>
2026-09-22 09:23:14 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,14 @@
+// Last updated: 22/09/2026, 09:23:13
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;        
+    }
+}
