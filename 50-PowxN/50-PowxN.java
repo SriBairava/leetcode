df5302444814b9728eb674b2e3a92c3e6f6874eb
SriBairava <sribairava001@gmail.com>
2026-09-22 09:23:27 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,24 @@
+// Last updated: 22/09/2026, 09:23:26
+class Solution {
+    public double myPow(double x, int n) {
+        
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        
+        double pow = 1;
+        
+        while(n != 0){
+            if((n & 1) != 0){
+                pow *= x;
+            } 
+                
+            x *= x;
+            n >>>= 1;
+            
+        }
+        
+        return pow;
+    }
+}
