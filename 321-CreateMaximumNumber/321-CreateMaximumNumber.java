4ea2e55ad69a8f7cc8be09bafded62f8aae328d7
SriBairava <sribairava001@gmail.com>
2026-09-24 10:36:37 +0530

    File updated at 24/09/2026, 10:36:36

--- 321-CreateMaximumNumber/321-CreateMaximumNumber.java
+++ 321-CreateMaximumNumber/321-CreateMaximumNumber.java
@@ -1,34 +1,47 @@
-// Last updated: 24/09/2026, 10:35:42
+// Last updated: 24/09/2026, 10:36:36
 1class Solution {
-2    public List<String> findItinerary(List<List<String>> tickets) {
-3        Map<String, List<String>> graph = new HashMap<>();
-4
-5        for (List<String> ticket : tickets) {
-6            String departure = ticket.get(0);
-7            String arrival = ticket.get(1);
-8
-9            graph.computeIfAbsent(departure, k -> new ArrayList<>()).add(arrival);
-10        }
-11
-12        for (List<String> destinations : graph.values()) {
-13            destinations.sort(Collections.reverseOrder());
-14        }
-15
-16        List<String> newItinerary = new ArrayList<>();
-17        Deque<String> stack = new ArrayDeque<>();
-18        stack.push("JFK");
-19
-20        while (!stack.isEmpty()) {
-21            String currentAirport = stack.peek();
-22
-23            if (graph.containsKey(currentAirport) && !graph.get(currentAirport).isEmpty()) {
-24                stack.push(graph.get(currentAirport).remove(graph.get(currentAirport).size() - 1));
-25            } else {
-26                newItinerary.add(stack.pop());
-27            }
-28        }
-29
-30        Collections.reverse(newItinerary);
-31        return newItinerary;        
-32    }
-33}
+2    public List<List<Integer>> palindromePairs(String[] words) {
+3        Map<String, Integer> wmap = new HashMap<>();
+4        List<List<Integer>> ans = new ArrayList<>();
+5        for (int i = 0; i < words.length; i++)
+6            wmap.put(words[i], i);
+7        for (int i = 0; i < words.length; i++) {
+8            if (words[i].equals("")) {
+9                for (int j = 0; j < words.length; j++) {
+10                    String w = words[j];
+11                    if (isPal(w, 0, w.length()-1) && j != i) {
+12                        ans.add(List.of(i, j));
+13                        ans.add(List.of(j, i));
+14                    }
+15                }
+16                continue;
+17            }
+18            StringBuilder sb = new StringBuilder(words[i]);
+19            sb.reverse();
+20            String bw = sb.toString();
+21            if (wmap.containsKey(bw)) {
+22                int res = wmap.get(bw);
+23                if (res != i) ans.add(List.of(i, res));
+24            }
+25            for (int j = 1; j < bw.length(); j++) {
+26                if (isPal(bw, 0, j-1)) {
+27                    String s = bw.substring(j);
+28                    if (wmap.containsKey(s))
+29                        ans.add(List.of(i, wmap.get(s)));
+30                }
+31                if (isPal(bw, j, bw.length()-1)) {
+32                    String s = bw.substring(0,j);
+33                    if (wmap.containsKey(s))
+34                        ans.add(List.of(wmap.get(s), i));
+35                }
+36            }
+37        }
+38        return ans;
+39    }
+40    
+41    private boolean isPal(String word, int i, int j) {
+42        while (i < j)
+43            if (word.charAt(i++) != word.charAt(j--)) return false;
+44        return true;
+45    }
+46}
