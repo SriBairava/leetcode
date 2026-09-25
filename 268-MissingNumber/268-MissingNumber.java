c8a02f6d612216f5e6acb9f43083f7baa66aaf3d
SriBairava <sribairava001@gmail.com>
2026-09-25 07:48:47 +0530

    File updated at 25/09/2026, 07:48:48

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,35 +1,16 @@
-// Last updated: 25/09/2026, 07:48:15
+// Last updated: 25/09/2026, 07:48:48
 1class Solution {
-2    public int islandPerimeter(int[][] grid) {
-3        int row = grid.length;
-4        int col = grid[0].length;
-5
-6        int perimeter = 0;
-7
-8        // Iterate through all cells in the grid 🗺️
-9        for (int i = 0; i < row; i++) {
-10            for (int j = 0; j < col; j++) {
-11                // Process only land cells 🏝️
-12                if (grid[i][j] == 1) {
-13                    // Check - UP ⬆️
-14                    if (i == 0 || grid[i - 1][j] == 0) {
-15                        perimeter++;
-16                    }
-17                    // Check - DOWN ⬇️
-18                    if (i == row - 1 || grid[i + 1][j] == 0) {
-19                        perimeter++;
-20                    }
-21                    // Check - LEFT ⬅️
-22                    if (j == 0 || grid[i][j - 1] == 0) {
-23                        perimeter++;
-24                    }
-25                    // Check - RIGHT ➡️
-26                    if (j == col - 1 || grid[i][j + 1] == 0) {
-27                        perimeter++;
-28                    }
-29                }
-30            }
-31        }
-32        return perimeter;
-33    }
-34}
+2    public int findComplement(int num) 
+3    {
+4        int i = 0;
+5        int j = 0;
+6        
+7        while (i < num)
+8        {
+9            i += Math.pow(2, j);
+10            j++;
+11        }
+12        
+13        return i - num;
+14    }
+15}
