fb899c7f5dabd1440bd235f0f4d10cd396d47e11
SriBairava <sribairava001@gmail.com>
2026-09-25 07:42:11 +0530

    File updated at 25/09/2026, 07:42:12

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,17 +1,23 @@
-// Last updated: 25/09/2026, 07:41:42
-1class Solution {
-2    public void reverseString(char[] s) {
-3        int left = 0;
-4        int right = s.length - 1;
-5
-6        while(left < right){
-7            char temp = s[left];
-8            s[left] = s[right];
-9            s[right] = temp;
-10
-11            left++;
-12            right--;
-13        }
-14       
-15    }
-16}
+// Last updated: 25/09/2026, 07:42:12
+1import java.util.*;
+2
+3class Solution {
+4    public int[] intersect(int[] nums1, int[] nums2) {
+5        Arrays.sort(nums1);
+6        Arrays.sort(nums2);
+7
+8        int i = 0, j = 0, k = 0;
+9        while (i < nums1.length && j < nums2.length) {
+10            if (nums1[i] == nums2[j]) {
+11                nums1[k++] = nums1[i];
+12                i++;
+13                j++;
+14            } else if (nums1[i] < nums2[j]) {
+15                i++;
+16            } else {
+17                j++;
+18            }
+19        }
+20        return Arrays.copyOfRange(nums1, 0, k);
+21    }
+22}
