621498f4e06d04ef6191bd2101e97065ed79dd39
SriBairava <sribairava001@gmail.com>
2026-09-25 08:04:24 +0530

    File updated at 25/09/2026, 08:04:24

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,28 +1,21 @@
-// Last updated: 25/09/2026, 08:03:32
+// Last updated: 25/09/2026, 08:04:24
 1class Solution {
-2    public ListNode rotateRight(ListNode head, int k) {
-3        if (head == null) return head;
-4
-5        int length = 1;
-6        ListNode dummy = head;
-7
-8        while (dummy.next != null) {
-9            dummy = dummy.next;
-10            length++;
+2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+3        int []res=new int[nums1.length];
+4        HashMap<Integer,Integer> map=new HashMap<>();
+5        Stack<Integer> st=new Stack<>();
+6        for(int i=0;i<nums2.length;i++){
+7            while(!st.isEmpty()&&st.peek()<nums2[i]){
+8                map.put(st.pop(),nums2[i]);
+9            }
+10            st.push(nums2[i]);
 11        }
-12
-13        int position = k % length;
-14        if (position == 0) return head;
-15
-16        ListNode current = head;
-17        for (int i = 0; i < length - position - 1; i++) {
-18            current = current.next;
-19        }
-20
-21        ListNode newHead = current.next;
-22        current.next = null;
-23        dummy.next = head;
-24
-25        return newHead;        
-26    }
-27}
+12        for(int i:st){
+13            map.put(i,-1);
+14        }
+15        for(int i=0;i<nums1.length;i++){
+16            res[i]=map.get(nums1[i]);
+17        }
+18        return res;
+19    }
+20}
