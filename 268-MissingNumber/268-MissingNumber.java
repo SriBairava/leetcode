9ac3fbc569b172db67e7d42e24a5b093e418a8a7
SriBairava <sribairava001@gmail.com>
2026-09-25 07:49:39 +0530

    File updated at 25/09/2026, 07:49:39

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,21 +1,13 @@
-// Last updated: 25/09/2026, 07:49:16
+// Last updated: 25/09/2026, 07:49:39
 1class Solution {
-2    public String licenseKeyFormatting(String s, int k) {
-3        int count = 0;
-4        s = s.toUpperCase();
-5        StringBuilder sb = new StringBuilder();
-6        for(int i = s.length() - 1; i >= 0; i--){
-7            char ch = s.charAt(i);
-8            if(ch == '-'){
-9                continue;
-10            }
-11            if(count == k){
-12                sb.append('-');
-13                count = 0;
-14            }
-15            sb.append(ch);
-16            count++; 
-17        }
-18        return sb.reverse().toString();
-19    }
-20}
+2    public int[] constructRectangle(int area) {
+3
+4        int width = (int) Math.sqrt(area);
+5
+6        while (area % width != 0) {
+7            width--;
+8        }
+9
+10        return new int[] { area / width, width };
+11    }
+12}
