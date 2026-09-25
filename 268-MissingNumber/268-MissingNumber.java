ac6f7a9deb72d2702230f3e948a0b223da9da9b1
SriBairava <sribairava001@gmail.com>
2026-09-25 07:41:41 +0530

    File updated at 25/09/2026, 07:41:42

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,24 +1,17 @@
-// Last updated: 25/09/2026, 07:41:15
-1class NumArray {
-2    int prefix[];
-3    public NumArray(int[] nums) {
-4        int n = nums.length;
-5        prefix=new int[nums.length];
-6        prefix[0] = nums[0];
-7        for(int i=1;i<n;i++){
-8            prefix[i] = prefix[i-1]+nums[i];
-9        }       
-10    }
-11    
-12    public int sumRange(int left, int right) {
-13        if(left==0)
-14            return prefix[right];
-15        return prefix[right] - prefix[left-1];
-16    }
-17}
-18
-19/**
-20 * Your NumArray object will be instantiated and called as such:
-21 * NumArray obj = new NumArray(nums);
-22 * int param_1 = obj.sumRange(left,right);
-23 */
+// Last updated: 25/09/2026, 07:41:42
+1class Solution {
+2    public void reverseString(char[] s) {
+3        int left = 0;
+4        int right = s.length - 1;
+5
+6        while(left < right){
+7            char temp = s[left];
+8            s[left] = s[right];
+9            s[right] = temp;
+10
+11            left++;
+12            right--;
+13        }
+14       
+15    }
+16}
