9139be72fb26806462c166b46ff6578f2855ef6b
SriBairava <sribairava001@gmail.com>
2026-09-25 07:37:00 +0530

    File updated at 25/09/2026, 07:37:01

--- 190-ReverseBits/190-ReverseBits.java
+++ 190-ReverseBits/190-ReverseBits.java
@@ -1,15 +1,11 @@
-// Last updated: 25/09/2026, 07:36:41
-1public class Solution {
-2    
-3    public int reverseBits(int num) {
-4        
-5        num = ((num & 0xffff0000) >>> 16) | ((num & 0x0000ffff) << 16);
-6        num = ((num & 0xff00ff00) >>> 8) | ((num & 0x00ff00ff) << 8);
-7        num = ((num & 0xf0f0f0f0) >>> 4) | ((num & 0x0f0f0f0f) << 4);
-8        num = ((num & 0xcccccccc) >>> 2) | ((num & 0x33333333) << 2);
-9        num = ((num & 0xaaaaaaaa) >>> 1) | ((num & 0x55555555) << 1);
-10        
-11        return num;
-12        
-13    }
-14}
+// Last updated: 25/09/2026, 07:37:01
+1class Solution {
+2    public static int hammingWeight(int n) {
+3	int ones = 0;
+4    	while(n!=0) {
+5    		ones = ones + (n & 1);
+6    		n = n>>>1;
+7    	}
+8    	return ones;
+9}
+10}
