145ab9e4e3f04771987662bb03c9921c16ae53dc
SriBairava <sribairava001@gmail.com>
2026-09-25 08:06:58 +0530

    File updated at 25/09/2026, 08:06:58

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,32 +1,24 @@
-// Last updated: 25/09/2026, 08:06:43
-1public class Solution {
-2    public String[] findRelativeRanks(int[] nums) {
-3        int[][] pair = new int[nums.length][2];
-4        
-5        for (int i = 0; i < nums.length; i++) {
-6            pair[i][0] = nums[i];
-7            pair[i][1] = i;
-8        }
-9        
-10        Arrays.sort(pair, (a, b) -> (b[0] - a[0]));
-11        
-12        String[] result = new String[nums.length];
-13
-14        for (int i = 0; i < nums.length; i++) {
-15            if (i == 0) {
-16                result[pair[i][1]] = "Gold Medal";
-17            }
-18            else if (i == 1) {
-19                result[pair[i][1]] = "Silver Medal";
-20            }
-21            else if (i == 2) {
-22                result[pair[i][1]] = "Bronze Medal";
-23            }
-24            else {
-25                result[pair[i][1]] = (i + 1) + "";
-26            }
-27        }
-28
-29        return result;
-30    }
-31}
+// Last updated: 25/09/2026, 08:06:58
+1class Solution {
+2    public boolean checkPerfectNumber(int num) {
+3
+4        if ((num & 1) == 1) {
+5            return false;
+6        }
+7
+8        int sum = 1;
+9
+10        for (int i = 2; i <= Math.sqrt(num); i++) {
+11
+12            if (num % i == 0) {
+13                sum += i;
+14
+15                if (i != num / i) {
+16                    sum += num / i;
+17                }
+18            }
+19        }
+20
+21        return sum == num;
+22    }
+23}
