1c1cbb3962cd878ffda404d25459263c7fc37735
SriBairava <sribairava001@gmail.com>
2026-09-25 08:05:34 +0530

    File updated at 25/09/2026, 08:05:34

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,36 +1,6 @@
-// Last updated: 25/09/2026, 08:05:01
-1public class Solution {
-2    Integer prev = null;
-3    int count = 1;
-4    int max = 0;
-5    public int[] findMode(TreeNode root) {
-6        if (root == null) return new int[0];
-7        
-8        List<Integer> list = new ArrayList<>();
-9        traverse(root, list);
-10        
-11        int[] res = new int[list.size()];
-12        for (int i = 0; i < list.size(); ++i) res[i] = list.get(i);
-13        return res;
-14    }
-15    
-16    private void traverse(TreeNode root, List<Integer> list) {
-17        if (root == null) return;
-18        traverse(root.left, list);
-19        if (prev != null) {
-20            if (root.val == prev)
-21                count++;
-22            else
-23                count = 1;
-24        }
-25        if (count > max) {
-26            max = count;
-27            list.clear();
-28            list.add(root.val);
-29        } else if (count == max) {
-30            list.add(root.val);
-31        }
-32        prev = root.val;
-33        traverse(root.right, list);
-34    }
-35}
+// Last updated: 25/09/2026, 08:05:34
+1class Solution {
+2    public String convertToBase7(int num) {
+3        return Integer.toString(num, 7);
+4    }
+5}
