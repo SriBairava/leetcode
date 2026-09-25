3813bf4dc6a5bc4a0bee8e4b00040a10607ca3fd
SriBairava <sribairava001@gmail.com>
2026-09-25 07:54:08 +0530

    File updated at 25/09/2026, 07:54:08

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,28 +1,41 @@
-// Last updated: 25/09/2026, 07:53:37
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode removeNthFromEnd(ListNode head, int n) {
-13        ListNode a = new ListNode(0);
-14        a.next = head;
-15        ListNode fast = a;
-16        for (int i = 0; i <= n; i++) {
-17            fast = fast.next;
-18        }
-19        ListNode slow = a;
-20        while(fast!=null){
-21            fast=fast.next;
-22            slow=slow.next;
-23        }
-24        slow.next = slow.next.next;
-25        return a.next;
-26    }
-27}
+// Last updated: 25/09/2026, 07:54:08
+1class Solution {
+2    public int divide(int dividend, int divisor) {
+3        long a = dividend;
+4        long b = divisor;
+5
+6        boolean negative = (a < 0) ^ (b < 0);
+7
+8        a = Math.abs(a);
+9        b = Math.abs(b);
+10
+11        long quotient = 0;
+12
+13        while (a >= b) {
+14            long currentDivisor = b;
+15            long multiple = 1;
+16
+17            while (a >= currentDivisor + currentDivisor) {
+18                currentDivisor += currentDivisor;
+19                multiple += multiple;
+20            }
+21
+22            a -= currentDivisor;
+23            quotient += multiple;
+24        }
+25
+26        if (negative) {
+27            quotient = -quotient;
+28        }
+29
+30        if (quotient > Integer.MAX_VALUE) {
+31            return Integer.MAX_VALUE;
+32        }
+33
+34        if (quotient < Integer.MIN_VALUE) {
+35            return Integer.MIN_VALUE;
+36        }
+37
+38        return (int) quotient;
+39    }
+40}
