e2cc244b3a2ac25af099630a02e877aadae138c5
SriBairava <sribairava001@gmail.com>
2026-09-25 07:50:15 +0530

    File updated at 25/09/2026, 07:50:15

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,13 +1,21 @@
-// Last updated: 25/09/2026, 07:49:39
+// Last updated: 25/09/2026, 07:50:15
 1class Solution {
-2    public int[] constructRectangle(int area) {
-3
-4        int width = (int) Math.sqrt(area);
+2    public int findMaxConsecutiveOnes(int[] nums) {
+3        int res = 0;
+4        int count = 0;
 5
-6        while (area % width != 0) {
-7            width--;
-8        }
-9
-10        return new int[] { area / width, width };
-11    }
-12}
+6        for (int n : nums) {
+7            if (n == 0) {
+8                count = 0;
+9            } else {
+10                count++;
+11            }
+12
+13            if (res < count) {
+14                res = count;
+15            }
+16        }
+17
+18        return res;        
+19    }
+20}
