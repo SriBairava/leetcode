30847be782b0d50779e0f30c8cac6af8614fa5c0
SriBairava <sribairava001@gmail.com>
2026-09-25 07:53:37 +0530

    File updated at 25/09/2026, 07:53:37

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,26 +1,28 @@
-// Last updated: 25/09/2026, 07:53:19
-1class Solution {
-2    public int threeSumClosest(int[] nums, int target) {
-3        Arrays.sort(nums);
-4        int n = nums.length;
-5        int result = nums[0] + nums[1] + nums[2]; // Initial best guess
-6
-7        for (int i = 0; i < n - 2; i++) {
-8            int left = i + 1, right = n - 1;
-9
-10            while (left < right) {
-11                int sum = nums[i] + nums[left] + nums[right];
-12
-13                if (Math.abs(target - sum) < Math.abs(target - result)) {
-14                    result = sum;
-15                }
-16
-17                if (sum == target) return target;
-18                else if (sum < target) left++;
-19                else right--;
-20            }
-21        }
-22
-23        return result;
-24    }
-25}
+// Last updated: 25/09/2026, 07:53:37
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeNthFromEnd(ListNode head, int n) {
+13        ListNode a = new ListNode(0);
+14        a.next = head;
+15        ListNode fast = a;
+16        for (int i = 0; i <= n; i++) {
+17            fast = fast.next;
+18        }
+19        ListNode slow = a;
+20        while(fast!=null){
+21            fast=fast.next;
+22            slow=slow.next;
+23        }
+24        slow.next = slow.next.next;
+25        return a.next;
+26    }
+27}
