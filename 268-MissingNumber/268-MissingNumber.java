eae6f04ee4e6652f96e254a56c6f743e252e07f1
SriBairava <sribairava001@gmail.com>
2026-09-25 07:43:46 +0530

    File updated at 25/09/2026, 07:43:46

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,24 +1,20 @@
-// Last updated: 25/09/2026, 07:42:48
+// Last updated: 25/09/2026, 07:43:46
 1class Solution {
-2    public List<String> readBinaryWatch(int turnedOn) {
-3        List<String> result = new ArrayList<>();
-4        
-5        for (int hour = 0; hour < 12; hour++) {
-6            
-7            for (int minute = 0; minute < 60; minute++) {
-8                
-9                int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
-10                
-11                if (totalBits == turnedOn) {
-12                    
-13                    String time = hour + ":" + 
-14                                  (minute < 10 ? "0" + minute : minute);
-15                    
-16                    result.add(time);
-17                }
-18            }
-19        }
-20        
-21        return result;
-22    }
-23}
+2    public String toHex(int num) {
+3
+4        if (num == 0) {
+5            return "0";
+6        }
+7
+8        StringBuilder sb = new StringBuilder(8);
+9        char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+10
+11        while (num != 0) {
+12
+13            sb.append(hex[num & 0b1111]);
+14            num >>>= 4;
+15        }
+16
+17        return sb.reverse().toString();
+18    }
+19}
