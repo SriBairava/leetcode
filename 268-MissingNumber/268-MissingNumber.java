63daa7811fa486dd119ba57b4b7b1d2ecd501708
SriBairava <sribairava001@gmail.com>
2026-09-25 07:49:16 +0530

    File updated at 25/09/2026, 07:49:16

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,16 +1,21 @@
-// Last updated: 25/09/2026, 07:48:48
+// Last updated: 25/09/2026, 07:49:16
 1class Solution {
-2    public int findComplement(int num) 
-3    {
-4        int i = 0;
-5        int j = 0;
-6        
-7        while (i < num)
-8        {
-9            i += Math.pow(2, j);
-10            j++;
-11        }
-12        
-13        return i - num;
-14    }
-15}
+2    public String licenseKeyFormatting(String s, int k) {
+3        int count = 0;
+4        s = s.toUpperCase();
+5        StringBuilder sb = new StringBuilder();
+6        for(int i = s.length() - 1; i >= 0; i--){
+7            char ch = s.charAt(i);
+8            if(ch == '-'){
+9                continue;
+10            }
+11            if(count == k){
+12                sb.append('-');
+13                count = 0;
+14            }
+15            sb.append(ch);
+16            count++; 
+17        }
+18        return sb.reverse().toString();
+19    }
+20}
