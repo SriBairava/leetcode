2e7ce0b064372cb4035ac80c42a9d2f21f08445f
SriBairava <sribairava001@gmail.com>
2026-09-25 07:44:31 +0530

    File updated at 25/09/2026, 07:44:31

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,20 +1,7 @@
-// Last updated: 25/09/2026, 07:43:46
+// Last updated: 25/09/2026, 07:44:31
 1class Solution {
-2    public String toHex(int num) {
-3
-4        if (num == 0) {
-5            return "0";
-6        }
-7
-8        StringBuilder sb = new StringBuilder(8);
-9        char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-10
-11        while (num != 0) {
-12
-13            sb.append(hex[num & 0b1111]);
-14            num >>>= 4;
-15        }
-16
-17        return sb.reverse().toString();
-18    }
-19}
+2        public boolean repeatedSubstringPattern(String str) {
+3        String s = str + str;
+4        return s.substring(1, s.length() - 1).contains(str);
+5    }
+6}
